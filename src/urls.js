973f88718b31f91990e363ae019a5cd193261c9b
phamdai <daipham952@gmail.com>
2023-01-06 19:41:46 +0700

    first commit

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,5 @@
-export const api='https://zingmp3server-production.up.railway.app/api'
-export const originURL='https://zingmp3server-production.up.railway.app'
+export const api='https://zingmp3-1qrf.onrender.com/api'
+export const originURL='https://zingmp3-1qrf.onrender.com'
 export const LoginsocialURL=`${api}/v1/oauth/login`
 export const refreshTokenURL=`${api}/v1/refresh`
 export const verifyemailURL=`${api}/v3/verify/email`

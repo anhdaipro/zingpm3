53ae9fdeefe7688ec8c3b07e4c3f486c0165ce7a
phamdai <daipham952@gmail.com>
2023-01-12 17:22:53 +0700

    first-commit

--- src/components/Player.js
+++ src/components/Player.js
@@ -131,7 +131,7 @@ const Player=()=>{
             dispatch(setsong({duration:0,time:{seconds:0,minutes:0}}))
         }
     })()
-    },[song,dispatch,songs])
+    },[song.file,dispatch])
    
     useEffect(() => {
         const now=() =>dayjs()

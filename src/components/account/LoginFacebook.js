5ee9a4f869b44186ebd040ea8b2ece59edfb098f
phamdai <daipham952@gmail.com>
2023-01-16 08:52:27 +0700

    first-commit

--- src/components/account/LoginFacebook.js
+++ src/components/account/LoginFacebook.js
@@ -43,12 +43,7 @@ const LoginFacebook=()=>{
             localStorage.setItem('token',access);
             const search = window.location.search;
             const params = new URLSearchParams(search);
-            if(params.get('next')!=null){
-                window.location.href=params.get('next')
-            }
-            else{
-                window.location.href='/'
-            }
+            
         }
         catch(e){
             console.log(e)
